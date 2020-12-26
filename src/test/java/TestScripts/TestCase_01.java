ab1fc4c01a780d0c34bd48a943970fc54cb2cf82
karthikkutala <karthikkutala@karthiks-mbp>
2020-12-25 20:26:15 -0600

    intial commit

---
+++ src/test/java/TestScripts/TestCase_01.java
@@ -0,0 +1,15 @@
+package TestScripts;
+
+public class TestCase_01 {
+
+	public static void main(String[] args) {
+		
+
+		
+		desiredcapabilities cap = new desiredcapabilities();
+	
+		
+		
+	}
+
+}
